4670b56b76a5771eaa2ee6ff10e39bc73bcd4198
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-04-10 15:38:03 +0900

    show sample items

--- pages/items/index.js
+++ pages/items/index.js
@@ -1,7 +1,18 @@
 function Items() {
+  const items = [
+    { id: 1, title: 'サンプル1' },
+    { id: 2, title: 'サンプル2' },
+    { id: 3, title: 'サンプル3' },
+  ]
+
   return (
     <div className="container">
       <h1>Hello</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
