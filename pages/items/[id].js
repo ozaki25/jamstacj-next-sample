8a3be40d2ee9a5147cffac5687a7b1a1e038f472
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-19 13:42:06 +0900

    add unstable_revalidate

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,9 +1,11 @@
 import { getItems, getItem } from 'api/QiitaApi'
 import { Container } from 'react-bootstrap'
 
-function Item({ item }) {
+function Item({ item, date }) {
+  if (!item) return <p>...loading</p>
   return (
     <Container>
+      <p>{date}</p>
       <h1>{item.title}</h1>
       <hr />
       <div dangerouslySetInnerHTML={{ __html: item.body }}></div>
@@ -16,11 +18,14 @@ export default Item
 export async function getStaticPaths() {
   const data = await getItems()
   const paths = data.map((item) => `/items/${item.id}`)
-  return { paths, fallback: false }
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps({ params }) {
   const data = await getItem({ id: params.id })
   const item = { id: data.id, title: data.title, body: data.rendered_body }
-  return { props: { item } }
+  return {
+    props: { item, date: String(new Date()) },
+    unstable_revalidate: 1,
+  }
 }
